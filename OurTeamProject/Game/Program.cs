6988f4438e212e6fcc1a9709a374757e559c0a58
ghiri00 <ghirikei@gmail.com>
2026-02-01 09:39:56 +0200

    Set console output encoding to UTF-8

--- OurTeamProject/Game/Program.cs
+++ OurTeamProject/Game/Program.cs
@@ -6,6 +6,7 @@ namespace Game
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             GameEngine engine = new GameEngine();
             engine.Run();
         }
